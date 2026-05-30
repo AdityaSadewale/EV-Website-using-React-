d7756ec7bb01a79aeeb528d7aeb133b759722792
ADITYA_SADEWALE <adityasadewale@gmail.com>
2026-05-30 22:26:06 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import arrow_btn from '../../assets/arrow_btn.png'
 import play_icon from '../../assets/play_icon.png'
 import pause_icon from '../../assets/pause_icon.png'
 
+
 const Hero = ({
   heroData,
   setHeroCount,
